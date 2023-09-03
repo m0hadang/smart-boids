e719f4dbf453caa423a1ae947df2d729e2ca9a29
mohadang <ehdlaos@gmail.com>
2023-09-03 18:20:09 +0900

    리팩토링 : 함수 위치 이동, 게임 운영 bt 처리를 위한 enum 추가

--- src/boid.rs
+++ src/boid.rs
@@ -1,4 +1,5 @@
-use bonsai_bt::{BT, Event, RUNNING, Status::Success, UpdateArgs};
+use std::collections::HashMap;
+use bonsai_bt::{Action, Behavior, BT, Event, RUNNING, State, Status::Success, UpdateArgs};
 use ggez::mint;
 
 //algorithm stuff
@@ -49,11 +50,6 @@ impl Boid {
             bt,
         }
     }
-
-    fn distance(&self, boid: &Boid) -> f32 {
-        ((self.x - boid.x).powi(2) + (self.y - boid.y).powi(2)).sqrt()
-    }
-    // generate boids
     pub fn create_boids(
         bt: &BT<BoidAction, String, f32>,
         count: usize,
@@ -66,124 +62,143 @@ impl Boid {
             .take(count)
             .collect()
     }
-}
+    pub fn create_bt() -> Behavior<BoidAction> {
+        let avoid_others = Action(BoidAction::AvoidOthers);
+        let fly_towards_center = Action(BoidAction::FlyTowardsCenter);
+        let limit_speed = Action(BoidAction::LimitSpeed);
+        let match_velocity = Action(BoidAction::MatchVelocity);
+        let keep_within_bounds = Action(BoidAction::KeepWithinBounds);
 
-pub fn game_tick(dt: f32, cursor: mint::Point2<f32>, boid: &mut Boid, other_boids: Vec<Boid>) {
-    // proceed to next iteration in event loop
-    let e: Event = UpdateArgs { dt: dt.into() }.into();
+        // Run both behaviors in parallell, WhenAll will always return (Running, 0.0) because
+        // both behaviors would have to return (Success, dt) to the WhenAll condition to succeed.
+        let avoid_and_fly = bonsai_bt::WhenAll(vec![fly_towards_center, avoid_others]);
+        let behavior = bonsai_bt::While(
+            Box::new(avoid_and_fly),
+            // vec![Succees, Success, Running] -> sequence is always returning running
+            vec![match_velocity, limit_speed, keep_within_bounds],
+        );
+        behavior
+    }
+    pub fn game_tick(dt: f32, cursor: mint::Point2<f32>, boid: &mut Boid, other_boids: Vec<Boid>) {
+        // proceed to next iteration in event loop
+        let e: Event = UpdateArgs { dt: dt.into() }.into();
 
-    // unwrap bt for boid
-    let mut bt = boid.bt.clone();
-    let db = &*bt.get_blackboard().get_db();
-    let win_width: f32 = *db.get("win_width").unwrap();
-    let win_height: f32 = *db.get("win_height").unwrap();
+        // unwrap bt for boid
+        let mut bt = boid.bt.clone();
+        let db = &*bt.get_blackboard().get_db();
+        let win_width: f32 = *db.get("win_width").unwrap();
+        let win_height: f32 = *db.get("win_height").unwrap();
 
-    #[rustfmt::skip]
-    bt.state.tick(&e, &mut |args: bonsai_bt::ActionArgs<Event, BoidAction>| {
-        match args.action {
-            BoidAction::AvoidOthers => {
-                let avoid_factor = 0.5;
-                let mut move_x = 0.0;
-                let mut move_y = 0.0;
-                for other in &other_boids {
-                    let dist = boid.distance(other);
-                    if dist < MIN_DISTANCE && dist > 0.0 {
-                        move_x += boid.x - other.x;
-                        move_y += boid.y - other.y;
+        #[rustfmt::skip]
+        bt.state.tick(&e, &mut |args: bonsai_bt::ActionArgs<Event, BoidAction>| {
+            match args.action {
+                BoidAction::AvoidOthers => {
+                    let avoid_factor = 0.5;
+                    let mut move_x = 0.0;
+                    let mut move_y = 0.0;
+                    for other in &other_boids {
+                        let dist = boid.distance(other);
+                        if dist < MIN_DISTANCE && dist > 0.0 {
+                            move_x += boid.x - other.x;
+                            move_y += boid.y - other.y;
+                        }
                     }
-                }
-                boid.dx += move_x * avoid_factor;
-                boid.dy += move_y * avoid_factor;
+                    boid.dx += move_x * avoid_factor;
+                    boid.dy += move_y * avoid_factor;
 
-                RUNNING
-            }
-            BoidAction::FlyTowardsCenter => {
-                let centering_factor = 0.05; // adjust velocity by this %
-                let mut center_x = 0.0;
-                let mut center_y = 0.0;
-                let mut num_neighbors = 0.0;
-                for other in &other_boids {
-                    if boid.distance(other) < VISUAL_RANGE {
-                        center_x += other.x;
-                        center_y += other.y;
-                        num_neighbors += 1.0;
+                    RUNNING
+                }
+                BoidAction::FlyTowardsCenter => {
+                    let centering_factor = 0.05; // adjust velocity by this %
+                    let mut center_x = 0.0;
+                    let mut center_y = 0.0;
+                    let mut num_neighbors = 0.0;
+                    for other in &other_boids {
+                        if boid.distance(other) < VISUAL_RANGE {
+                            center_x += other.x;
+                            center_y += other.y;
+                            num_neighbors += 1.0;
+                        }
                     }
-                }
-                if num_neighbors > 0.0 {
-                    center_x /= num_neighbors;
-                    center_y /= num_neighbors;
+                    if num_neighbors > 0.0 {
+                        center_x /= num_neighbors;
+                        center_y /= num_neighbors;
 
-                    boid.dx += (center_x - boid.x) * centering_factor;
-                    boid.dy += (center_y - boid.y) * centering_factor;
-                }
-
-                RUNNING
-            }
-            BoidAction::MatchVelocity => {
-                let matching_factor = 0.1;
-                let mut avg_dx = 0.0;
-                let mut avg_dy = 0.0;
-                let mut num_neighbors = 0.0;
-                for other in &other_boids {
-                    if boid.distance(other) < VISUAL_RANGE {
-                        avg_dx += other.dx;
-                        avg_dy += other.dy;
-                        num_neighbors += 1.0;
+                        boid.dx += (center_x - boid.x) * centering_factor;
+                        boid.dy += (center_y - boid.y) * centering_factor;
                     }
-                }
-                if num_neighbors > 0.0 {
-                    avg_dx /= num_neighbors;
-                    avg_dy /= num_neighbors;
 
-                    boid.dx += (avg_dx - boid.dx) * matching_factor;
-                    boid.dy += (avg_dy - boid.dy) * matching_factor;
+                    RUNNING
                 }
-                (Success, args.dt)
+                BoidAction::MatchVelocity => {
+                    let matching_factor = 0.1;
+                    let mut avg_dx = 0.0;
+                    let mut avg_dy = 0.0;
+                    let mut num_neighbors = 0.0;
+                    for other in &other_boids {
+                        if boid.distance(other) < VISUAL_RANGE {
+                            avg_dx += other.dx;
+                            avg_dy += other.dy;
+                            num_neighbors += 1.0;
+                        }
+                    }
+                    if num_neighbors > 0.0 {
+                        avg_dx /= num_neighbors;
+                        avg_dy /= num_neighbors;
+
+                        boid.dx += (avg_dx - boid.dx) * matching_factor;
+                        boid.dy += (avg_dy - boid.dy) * matching_factor;
+                    }
+                    (Success, args.dt)
+                }
+                BoidAction::LimitSpeed => {
+                    let speed = (boid.dx * boid.dx + boid.dy * boid.dy).sqrt();
+                    if speed > SPEED_LIMIT {
+                        boid.dx = (boid.dx / speed) * SPEED_LIMIT;
+                        boid.dy = (boid.dy / speed) * SPEED_LIMIT;
+                    }
+
+                    (Success, args.dt)
+                }
+                BoidAction::KeepWithinBounds => {
+                    let edge_buffer: f32 = 40.0;
+                    let turn_factor: f32 = 16.0;
+                    let mut x_bounded = true;
+                    let mut y_bounded = true;
+
+                    if boid.x < win_width - edge_buffer {
+                        boid.dx += turn_factor;
+                        x_bounded = !x_bounded;
+                    }
+                    if boid.x > edge_buffer {
+                        boid.dx -= turn_factor;
+                        x_bounded = !x_bounded;
+                    }
+                    if boid.y < win_height - edge_buffer {
+                        boid.dy += turn_factor;
+                        y_bounded = !y_bounded
+                    }
+                    if boid.y > edge_buffer {
+                        boid.dy -= turn_factor;
+                        y_bounded = !y_bounded
+                    }
+                    if !x_bounded {
+                        boid.dx *= 0.8;
+                    }
+                    if !y_bounded {
+                        boid.dy *= 0.8;
+                    }
+                    if ((boid.x - cursor.x).powi(2) + (boid.y - cursor.y).powi(2)).sqrt() < 20.0 {
+                        boid.dx += (boid.x - cursor.x) * 1.0;
+                        boid.dy += (boid.y - cursor.y) * 1.0;
+                    }
+
+                    RUNNING
+                }
             }
-            BoidAction::LimitSpeed => {
-                let speed = (boid.dx * boid.dx + boid.dy * boid.dy).sqrt();
-                if speed > SPEED_LIMIT {
-                    boid.dx = (boid.dx / speed) * SPEED_LIMIT;
-                    boid.dy = (boid.dy / speed) * SPEED_LIMIT;
-                }
-
-                (Success, args.dt)
-            }
-            BoidAction::KeepWithinBounds => {
-                let edge_buffer: f32 = 40.0;
-                let turn_factor: f32 = 16.0;
-                let mut x_bounded = true;
-                let mut y_bounded = true;
-
-                if boid.x < win_width - edge_buffer {
-                    boid.dx += turn_factor;
-                    x_bounded = !x_bounded;
-                }
-                if boid.x > edge_buffer {
-                    boid.dx -= turn_factor;
-                    x_bounded = !x_bounded;
-                }
-                if boid.y < win_height - edge_buffer {
-                    boid.dy += turn_factor;
-                    y_bounded = !y_bounded
-                }
-                if boid.y > edge_buffer {
-                    boid.dy -= turn_factor;
-                    y_bounded = !y_bounded
-                }
-                if !x_bounded {
-                    boid.dx *= 0.8;
-                }
-                if !y_bounded {
-                    boid.dy *= 0.8;
-                }
-                if ((boid.x - cursor.x).powi(2) + (boid.y - cursor.y).powi(2)).sqrt() < 20.0 {
-                    boid.dx += (boid.x - cursor.x) * 1.0;
-                    boid.dy += (boid.y - cursor.y) * 1.0;
-                }
-
-                RUNNING
-            }
-        }
-    });
+        });
+    }
+    fn distance(&self, boid: &Boid) -> f32 {
+        ((self.x - boid.x).powi(2) + (self.y - boid.y).powi(2)).sqrt()
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 use std::collections::{HashMap, HashSet};
 
-use bonsai_bt::{Action, ActionArgs, BT, Event, Failure, RUNNING, Select, Sequence, State, Success, UpdateArgs};
-use ggez::{conf, Context, ContextBuilder, event, GameResult, graphics, input, mint, timer};
+use bonsai_bt::{ActionArgs, BT, Event, State, UpdateArgs, Success, Action, Failure, Sequence, Behavior};
+use ggez::{conf, Context, ContextBuilder, event, GameResult, graphics, input, timer};
 use ggez::mint::Point2;
 use ggez::winit::event::VirtualKeyCode;
-
-use boid::game_tick;
 
 use crate::boid::{Boid, BoidAction};
 
@@ -16,12 +14,17 @@ const WINDOW_WIDTH: f32 = WINDOW_HEIGHT * (16.0 / 9.0);
 const OBJECT_COUNT: usize = 100;
 pub const OBJECT_SIZE: f32 = 32.0; // Pixels
 
-#[derive(Clone, Debug, PartialEq)]
+#[derive(Clone, PartialEq)]
 enum PlayState {
-    InputKey,
-    Setup,
     Play,
+    Setup,
     Pause,
+}
+
+#[derive(Clone, Debug)]
+enum GameOpState {
+    InputKey,
+    UpdateGameData,
 }
 
 struct GameState {
@@ -30,14 +33,13 @@ struct GameState {
     boids: Vec<Boid>,
     points: Vec<glam::Vec2>,
     bt: BT<BoidAction, String, f32>,
-    game_op_bt: State<PlayState>,
+    game_op_bt: State<GameOpState>,
 }
 
 impl GameState {
     pub fn new(
         _ctx: &mut Context,
         bt: BT<BoidAction, String, f32>,
-        game_op_bt: State<PlayState>,
     ) -> GameState {
         GameState {
             state: PlayState::Setup,
@@ -50,19 +52,25 @@ impl GameState {
                 glam::vec2(-OBJECT_SIZE / 4.0, OBJECT_SIZE / 2.0),
             ],
             bt,
-            game_op_bt,
+            game_op_bt: Self::create_bt(),
         }
     }
-
+    fn create_bt() -> State<GameOpState> {
+        let state = Sequence(vec![
+            Action(GameOpState::InputKey),
+            Action(GameOpState::UpdateGameData)
+        ]);
+        State::new(state)
+    }
     fn game_op_tick(&mut self,
                     dt: f32,
                     pressed_keys: &HashSet<VirtualKeyCode>,
                     cursor: Point2<f32>) {
         let e: Event = UpdateArgs { dt: dt.into() }.into();
         let mut game_op_bt = self.game_op_bt.clone();
-        game_op_bt.tick(&e, &mut |args: ActionArgs<Event, PlayState>|
+        game_op_bt.tick(&e, &mut |args: ActionArgs<Event, GameOpState>|
             match args.action {
-                PlayState::InputKey => {
+                GameOpState::InputKey => {
                     if pressed_keys.is_empty() {
                     } else {
                         // -> setup
@@ -94,7 +102,6 @@ impl GameState {
                                         self.state = PlayState::Pause;
                                     }
                                 }
-                                _ => {}
                             };
                         }
                     }
@@ -105,12 +112,12 @@ impl GameState {
                         (Failure, args.dt)
                     }
                 }
-                PlayState::Play => {
+                GameOpState::UpdateGameData => {
                     let tick = (self.dt.subsec_millis() as f32) / 1000.0;
                     for i in 0..(self.boids).len() {
                         let boids_vec = self.boids.to_vec();
                         let boid = &mut self.boids[i];
-                        game_tick(
+                        Boid::game_tick(
                             self.dt.as_secs_f32(),
                             cursor,
                             boid,
@@ -125,7 +132,6 @@ impl GameState {
                     }
                     (Success, args.dt)
                 }
-                _ => RUNNING
             },
         );
     }
@@ -144,7 +150,6 @@ impl event::EventHandler for GameState {
             cursor);
         Ok(())
     }
-
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         graphics::clear(ctx, [0.15, 0.2, 0.22, 1.0].into());
         // MENU: display controls
@@ -211,44 +216,21 @@ impl event::EventHandler for GameState {
     }
 }
 
-fn create_bt() -> BT<BoidAction, String, f32> {
-    let avoid_others = Action(BoidAction::AvoidOthers);
-    let fly_towards_center = Action(BoidAction::FlyTowardsCenter);
-    let limit_speed = Action(BoidAction::LimitSpeed);
-    let match_velocity = Action(BoidAction::MatchVelocity);
-    let keep_within_bounds = Action(BoidAction::KeepWithinBounds);
-
-    // Run both behaviors in parallell, WhenAll will always return (Running, 0.0) because
-    // both behaviors would have to return (Success, dt) to the WhenAll condition to succeed.
-    let avoid_and_fly = bonsai_bt::WhenAll(vec![fly_towards_center, avoid_others]);
-    let behavior = bonsai_bt::While(
-        Box::new(avoid_and_fly),
-        // vec![Succees, Success, Running] -> sequence is always returning running
-        vec![match_velocity, limit_speed, keep_within_bounds],
-    );
-
-    // add some values to blackboard
-    let mut blackboard: HashMap<String, f32> = HashMap::new();
-    blackboard.insert("win_width".to_string(), WINDOW_WIDTH);
-    blackboard.insert("win_height".to_string(), WINDOW_HEIGHT);
-
-    // create bt
-    BT::new(behavior, blackboard)
-}
-
 fn main() {
     let (mut ctx, events_loop) = ContextBuilder::new("Boids", "Daniel Eisen")
         .window_mode(conf::WindowMode::default().dimensions(WINDOW_WIDTH, WINDOW_HEIGHT))
         .window_setup(conf::WindowSetup::default().samples(conf::NumSamples::Eight))
         .build()
         .expect("Failed to create context");
-    let bt = create_bt();
 
-    let play = Action(PlayState::Play);
-    let input_key = Action(PlayState::InputKey);
-    let root = Sequence(vec![input_key, play]);
-    let game_op_bt = State::new(root);
+    let boid_bt = Boid::create_bt();
+    let mut blackboard: HashMap<String, f32> = HashMap::new();
+    blackboard.insert("win_width".to_string(), WINDOW_WIDTH);
+    blackboard.insert("win_height".to_string(), WINDOW_HEIGHT);
+    let boid_bt: BT<BoidAction, String, f32> = BT::new(boid_bt, blackboard);
 
-    let game_state = GameState::new(&mut ctx, bt, game_op_bt);
+    let game_state =
+        GameState::new(&mut ctx, boid_bt);
     event::run(ctx, events_loop, game_state);
 }
+
